e32225e156eecfc67f60412103c10d2ee581ef91
girishpanchal30 <girish@krishaweb.com>
2023-03-27 14:42:23 +0530

    Fix img field cumulates price issue for non-english title Codeinwp/ppom-pro#171

--- inc/functions.php
+++ inc/functions.php
@@ -1370,6 +1370,10 @@ function ppom_get_option_id( $option, $field_meta = null ) {
 		case 'image':
 			$the_option   = isset( $option['title'] ) ? $option['title'] : '';
 			$option['id'] = sanitize_key( $the_option );
+			if ( empty( $option['id'] ) && is_scalar( $the_option ) ) {
+				$the_option   = strtolower( $the_option );
+				$option['id'] = wp_hash( $the_option );
+			}
 			break;
 
 		case 'imageselect':
