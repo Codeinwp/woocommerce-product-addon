7988614e3e6a9275456f0eeade3d582b15749e35
Soare Robert-Daniel <soare.robert.daniel@protonmail.com>
2026-05-13 12:08:17 +0300

    refactor: remove PRO badge in CCS/JS

--- templates/admin/ppom-fields.php
+++ templates/admin/ppom-fields.php
@@ -293,15 +293,6 @@ $fields_groups = function_exists( 'ppom_get_admin_field_type_groups' ) ? ppom_ge
 							<div class="col-md-6 col-sm-12">
 								<div class="form-group">
 									<label><?php _e( 'Custom CSS', 'woocommerce-product-addon' ); ?>
-										<?php if ( $is_legacy_user ) : ?>
-											<span class="ppom-pro-badge">
-												<svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ff0000">
-													<path d="M19 11H5C3.89543 11 3 11.8954 3 13V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V13C21 11.8954 20.1046 11 19 11Z" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-													<path d="M7 11V7C7 5.67392 7.52678 4.40215 8.46447 3.46447C9.40215 2.52678 10.6739 2 12 2C13.3261 2 14.5979 2.52678 15.5355 3.46447C16.4732 4.40215 17 5.67392 17 7V11" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-												</svg>
-												<?php esc_html_e( 'PRO', 'woocommerce-product-addon' ); ?>
-											</span>
-										<?php endif; ?>
 										<span class="ppom-helper-icon" data-ppom-tooltip="ppom_tooltip"
 												title="<?php _e( 'Add your own CSS.', 'woocommerce-product-addon' ); ?>"><i
 													class="dashicons dashicons-editor-help"></i></span>
@@ -337,15 +328,6 @@ $fields_groups = function_exists( 'ppom_get_admin_field_type_groups' ) ? ppom_ge
 							<div class="col-md-6 col-sm-12">
 								<div class="form-group">
 									<label><?php _e( 'Custom JavaScript', 'woocommerce-product-addon' ); ?>
-										<?php if ( $is_legacy_user ) : ?>
-											<span class="ppom-pro-badge">
-												<svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ff0000">
-													<path d="M19 11H5C3.89543 11 3 11.8954 3 13V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V13C21 11.8954 20.1046 11 19 11Z" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-													<path d="M7 11V7C7 5.67392 7.52678 4.40215 8.46447 3.46447C9.40215 2.52678 10.6739 2 12 2C13.3261 2 14.5979 2.52678 15.5355 3.46447C16.4732 4.40215 17 5.67392 17 7V11" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-												</svg>
-												<?php esc_html_e( 'PRO', 'woocommerce-product-addon' ); ?>
-											</span>
-										<?php endif; ?>
 										<span class="ppom-helper-icon" data-ppom-tooltip="ppom_tooltip"
 												title="<?php _e( 'Add your own JavaScript code.', 'woocommerce-product-addon' ); ?>"><i
 													class="dashicons dashicons-editor-help"></i></span>
